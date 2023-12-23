e27c35eefdb35d8a36837bb09e8ffaeee6550cbc
thititongumpun <thiti180536@gmail.com>
2023-12-23 22:57:18 +0700

    add secret api token

--- config/config.py
+++ config/config.py
@@ -4,5 +4,6 @@ from novu.api import EventApi
 
 class Settings(BaseSettings):
     novu_apikey: str
+    x_api_key: str
 
     model_config = SettingsConfigDict(env_file=".env")
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status, Header
 from functools import lru_cache
 from typing_extensions import Annotated
 from config import config
+from typing import Optional
 from model.payload import Payload
 from service import novu
 
@@ -19,7 +20,12 @@ async def root():
 
 
 @app.post("/notification")
-async def notification(settings: Annotated[config.Settings, Depends(get_settings)], payload: Payload):
+async def notification(settings: Annotated[config.Settings, Depends(get_settings)], payload: Payload, x_api_key: Optional[str] = Header(None)):
+    if (settings.x_api_key != x_api_key):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="no authorized",
+        )
     res = novu.Novu(settings.novu_apikey).trigger(
         "timesheet",
         "c761c317-2037-4315-8a1a-829523a98403",
