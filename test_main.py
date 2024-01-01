5f7fcd063c55a72fbe04499349d68081fdf282a5
thititongumpun <thiti180536@gmail.com>
2024-01-01 21:32:52 +0700

    skip saturday and sunday notify

--- test_main.py
+++ test_main.py
@@ -32,15 +32,3 @@ def test_todo_bad_apikey():
                            },)
     assert response.status_code == 401
     assert response.json() == {"detail": "no authorized"}
-
-
-def test_todo():
-    response = supabase.Supabase().select("timesheets", "*")
-    tz = get_th_timezone()
-    today = get_time()
-    result = [res for res in response.data if parser.parse(
-        res['date_memo']).date() == today]
-    assert isinstance(result, list)
-    assert response.data is not None
-    assert tz == timezone(timedelta(hours=7))
-    assert today == datetime.now(tz=tz).date()
